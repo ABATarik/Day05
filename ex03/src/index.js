fefed44e6ce9d712b58ecf70bce5e1159012e26e
ABATarik <abaspahic.tarik@outlook.com>
2021-03-12 18:55:18 +0100

    Escape Sequences in String

---
+++ ex03/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    var myString;
+
+    myString = "FirstLine\n\tSecondLine\nThirdLine\n"
+
+    return myString;
+    
+    
+}
+
+console.log(main());
+
+module.exports = main;
