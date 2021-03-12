413cb48d4a3e98df6ec9c7669f307edf37f4bc12
ABATarik <abaspahic.tarik@outlook.com>
2021-03-12 11:05:14 +0100

    Literal Quotes

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+function main() {
+     var firstString = "I think therefore  i \"am\" -- sentient" 
+     var secondstring = "no you only \"think\",so you aren`t"
+
+    return {
+     firstString,
+     secondstring
+
+
+    }
+}
+
+console.log(main());
+
+module.exports = main;
