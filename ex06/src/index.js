86306372d02c8022e07edcd8878f076af2819ef3
ABATarik <abaspahic.tarik@outlook.com>
2021-03-12 19:13:26 +0100

    Constructing String

---
+++ ex06/src/index.js
@@ -0,0 +1,12 @@
+function main() {
+    var myName = "Tarik";
+    var myString;
+    myString = "My name is " + myName + " and i am well."; 
+
+
+    return myString;
+}
+
+console.log(main());
+
+module.exports = main;
